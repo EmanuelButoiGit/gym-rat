dd8660ed8f01344e0fcb526a15ad7c3c4547a531
EmanuelButoiGit <emanuelb1000@gmail.com>
2024-02-05 17:43:49 +0200

    Update TestController.java

--- gym-rat-api/src/main/java/com/gym/rat/TestController.java
+++ gym-rat-api/src/main/java/com/gym/rat/TestController.java
@@ -1,7 +1,5 @@
 package com.gym.rat;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "Get message")
-    @ApiResponse(responseCode = "200", description = "The massage was retrieved")
     public String getAllRatings()
     {
         return "Hello Gym Rat!";
