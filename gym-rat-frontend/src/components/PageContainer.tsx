1acf3ba3433a17100116a27bfb7e97ec782a04c2
EmanuelButoiGit <emanuelb1000@gmail.com>
2024-02-09 23:23:23 +0200

    Update PageContainer.tsx

--- gym-rat-frontend/src/components/PageContainer.tsx
+++ gym-rat-frontend/src/components/PageContainer.tsx
@@ -1,13 +1,21 @@
 import { Container } from '@mui/material';
+import Footer from './Footer';
 
-const PageContainer = (props: any) => {
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-            <Container sx={{ backgroundColor: "azure", minHeight: "100vh", width: "95vh", overflow: "auto" }}>
-                {props.children}
-            </Container>
-        </div>
-    );
+const PageContainer = ( props: any ) => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Container component="main" 
+        sx={{ flex: 1, 
+            backgroundColor: "azure", 
+            paddingY: 2, 
+            minHeight: "100vh", 
+            width: "95vh", 
+            overflow: "auto" }}>
+        {props.children}
+      </Container>
+      <Footer />
+    </div>
+  );
 };
 
 export default PageContainer;
