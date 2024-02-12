f624f87706cf2fd3470da4f6bb8f5843a7669025
EmanuelButoiGit <emanuelb1000@gmail.com>
2024-02-12 22:28:07 +0200

    Create AchievementsDto.java

---
+++ gym-rat-api/src/main/java/com/gym/rat/AchievementsDto.java
@@ -0,0 +1,13 @@
+package com.gym.rat;
+
+import lombok.Data;
+
+@Data
+public class AchievementsDto {
+    boolean smile;
+    boolean cardio;
+    boolean bro;
+    boolean leg;
+    boolean brah;
+    boolean rest;
+}
