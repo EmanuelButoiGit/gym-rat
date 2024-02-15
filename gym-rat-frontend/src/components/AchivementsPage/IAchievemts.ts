03596cda4ec2a378da91f8784aed6a11fa506096
EmanuelButoiGit <emanuelb1000@gmail.com>
2024-02-15 15:35:46 +0200

    Create IAchievemts.ts

---
+++ gym-rat-frontend/src/components/AchivementsPage/IAchievemts.ts
@@ -0,0 +1,9 @@
+export default interface IAchievements {
+    id: number;
+    smile: boolean;
+    cardio: boolean;
+    bro: boolean;
+    leg: boolean;
+    brah: boolean;
+    rest: boolean;
+  }
