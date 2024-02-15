a12c40516e5298444b529db7219c884603cd6bfc
EmanuelButoiGit <emanuelb1000@gmail.com>
2024-02-15 15:31:03 +0200

    Refactor cross origin

--- gym-rat-api/src/main/java/com/gym/rat/controllers/AchievementController.java
+++ gym-rat-api/src/main/java/com/gym/rat/controllers/AchievementController.java
@@ -5,6 +5,7 @@ import com.gym.rat.dtos.AchievementsDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/achievements")
@@ -12,37 +13,31 @@ public class AchievementController {
 
     private final AchievementService achievementService;
 
-    @CrossOrigin
     @PostMapping("/smile")
     public void saveSmileAchievement() {
         achievementService.saveSmileAchievement();
     }
 
-    @CrossOrigin
     @PostMapping("/cardio")
     public void saveCardioAchievement() {
         achievementService.saveCardioAchievement();
     }
 
-    @CrossOrigin
     @PostMapping("/bro")
     public void saveBroAchievement() {
         achievementService.saveBroAchievement();
     }
 
-    @CrossOrigin
     @PostMapping("/leg")
     public void saveLegAchievement() {
         achievementService.saveLegAchievement();
     }
 
-    @CrossOrigin
     @PostMapping("/brah")
     public void saveBrahAchievement() {
         achievementService.saveBrahAchievement();
     }
 
-    @CrossOrigin
     @PostMapping("/rest")
     public void saveRestAchievement() {
         achievementService.saveRestAchievement();
