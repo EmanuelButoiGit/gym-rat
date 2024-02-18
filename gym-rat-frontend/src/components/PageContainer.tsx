817c42340787da1fc7510d0619de580296fcb920
EmanuelButoiGit <emanuelb1000@gmail.com>
2024-02-18 22:13:02 +0200

    Update PageContainer.tsx

--- gym-rat-frontend/src/components/PageContainer.tsx
+++ gym-rat-frontend/src/components/PageContainer.tsx
@@ -3,12 +3,12 @@ import Footer from './Footer';
 
 const PageContainer = ( props: any ) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100%' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Container component="main" 
         sx={{ flex: 1, 
             backgroundColor: "azure", 
             paddingY: 2, 
-            minHeight: "100%", 
+            minHeight: "100vh", 
             width: "95vh", 
             overflow: "auto" }}>
         {props.children}
